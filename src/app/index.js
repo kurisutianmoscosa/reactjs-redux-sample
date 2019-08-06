f6f8a71316ecd530e662bc2abc78c0d089f3bf92
kurisutian.moscosa <moscosa.tian@gmail.com>
2019-08-06 16:51:02 +0800

    Initial commit

---
+++ src/app/index.js
@@ -0,0 +1,78 @@
+import React from "react";
+import {render} from "react-dom";
+import {createStore, combineReducers, applyMiddleware} from "redux";
+import {createLogger as logger}  from "redux-logger";
+import {Provider} from "react-redux";
+
+import App from "./components/App";
+
+const mathReducer = (state = {
+    result: 1,
+    lastValues: []
+  }, action) => {
+    switch(action.type) {
+      
+      case "ADD":
+        state = {
+          ...state,
+          result: state.result + action.payload,
+          lastValues: [...state.lastValues, action.payload]
+        };
+        break
+      case "SUBTRACT":
+          state = {
+            ...state,
+            result: state.result - action.payload,
+            lastValues: [...state.lastValues, action.payload]
+          };
+        break;
+    }
+    return state;
+  };
+  
+  const userReducer = (state = {
+    name: "Christian",
+    age: 25
+  }, action) => {
+    switch(action.type) {
+      
+      case "SET_NAME":
+        state = {
+          ...state,
+          name: action.payload
+        };
+        break
+      case "SET_AGE":
+          state = {
+            ...state,
+            age: action.payload
+          };
+        break;
+    }
+    return state;
+  };
+  
+  const myLogger = (store) => (next) => (action) => {
+    console.log("Logged Action: ", action);
+    next(action);
+  };
+  
+  const store = createStore(
+      combineReducers({math: mathReducer, user: userReducer}), 
+      {}, 
+      applyMiddleware(logger()))
+  
+  store.subscribe(() => {
+    //console.log("Store updated!", store.getState());
+  });
+
+  store.dispatch({
+    type: "ADD",
+    payload: 1
+  });   
+  
+render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    window.document.getElementById('app'));
